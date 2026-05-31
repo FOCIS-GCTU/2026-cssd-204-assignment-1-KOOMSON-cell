3e2da5115e5408869aa64d67eec035a5d653b993
KOOMSON-Cell <koomsonjstc@gmail.com>
2026-05-31 12:27:30 +0000

    new commit

--- initials.py
+++ initials.py
@@ -1,7 +1,42 @@
+# file: initials.py
+# Discription: Print out my initials.
+# Assignment Number: 1
+#
+# Name: <Koomson Kwesi Justice>
+# STUDENT ID: <2425403190>
+# Email: <koomsonjstc@gmail.com>
+# Grader: <Augustus>
+#
+# On my honor, <Koomson Kwesi Justice>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large letters.
+    
+    print()
+    
+    
+    print("...KKJ")
+    
+    
+    print()
+    
+    
+    print("...KKKKKKKKKKKK...KKKKKKKKKKKK......****")
+    print("...KKK......KKK...KKK......KKK.........   JJJ...****")
+    print("...KKK....KKKK....KKK....KKKK..........   JJJ...****")
+    print("...KKK..KKKK......KKK..KKKK.....    ...   JJJ...****")
+    print("...KKKKKKK........KKKKKKK........   ...   JJJ...****")
+    print("...KKKKKKK........KKKKKKK.............    jjj...****")
+    print("...KKK..KKKK......KKK..KKKK......JJJ..   .JJJ...****")
+    print("...KKK....KKKK....KKK....KKKK....JJJ..    JJJ...****")
+    print("...KKK......KKK...KKK......KKK...JJJ......JJJ...****")
+    print("...KKK......KKK...KKK......KKK...JJJ......JJJ...****")
+    
+    print()
+    
+    
+    main()
 
 
-
-main()
